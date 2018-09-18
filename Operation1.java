cd7afd9a89ea044fbf9009bbb5fc0321c76cd3b4
David Brungardt <34960989+brungardtdb@users.noreply.github.com>
2018-09-17 19:13:55 -0600

    Initial commit

---
+++ Operation1.java
@@ -0,0 +1,15 @@
+/* Operation1.java
+Author: David Brungardt
+
+This program prints:
+"My name is {name goes here}"
+"My favourite movie is {movie title goes here} */
+
+public class Operation1
+{
+  public static void main(String[] args)
+  {
+    // Instantiate new instance of PrintName class and pass string values to constructor
+    PrintName ThisName = new PrintName("David Brungardt","A Beary Scary Movie");
+  }// End of main method
+}// End of Operation1 class
---
+++ PrintName.java
@@ -0,0 +1,17 @@
+/* PrintName.java
+Author: David Brungardt
+
+This program accepts two strings as input and
+prints the user name and favourite movie title  */
+
+public class PrintName
+{
+// Constructor for PrintName class
+  public PrintName(String name, String movieTitle)
+  {
+    // Print introduction
+    System.out.println("My name is " + name);
+    // Print favorite movie title
+    System.out.println("My favorite movie is " + movieTitle);
+  }// End of consructor
+}// End of PrintName class
